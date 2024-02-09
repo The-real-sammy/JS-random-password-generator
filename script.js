5eaf3a2e8cbef3b1d9987cc20cc3778359061ca7
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-02-09 17:43:46 +0000

    adding other characters

---
+++ script.js
@@ -0,0 +1,154 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  // determine the number of characters 
+  var passwordLength = prompt("How many characters would you like in your password? (8-128)");
+//  
+  var error = true;
+  while (error) {
+    if (passwordLength < 8 || passwordLength > 128) {
+//create alert for was word length, if true create prompt for number of characters again
+      alert("Please input a number between 8 and 128.");
+      passwordLength = prompt("How many characters would you like in your password? (8-128)");
+    } 
+    //if the error is false create prompt for characters types by calling on the arrays above 
+    else {
+      error = false;
+      var charType = [{
+        type: "special characters",
+        character: specialCharacters,
+        incl_char_type: false
+      },
+      {
+        type: "numeric characters",
+        character: numericCharacters,
+        incl_char_type: false
+      },
+      {
+        type: "lowercase characters",
+        character: lowerCasedCharacters,
+        incl_char_type: false
+      },
+      {
+        type: "uppercase characters",
+        character: upperCasedCharacters,
+        incl_char_type: false
+      }];
+    }}}
+
+// console.log("is this the options", getPasswordOptions)
+// console.log("i think this is the prompt?", passwordLength)
+
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
