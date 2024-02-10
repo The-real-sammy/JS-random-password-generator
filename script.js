fe48965b74de9fbb025e922eb6f9b0ed1ba09f84
The-real-sammy <152024562+The-real-sammy@users.noreply.github.com>
2024-02-10 07:34:39 +0000

    funnction added to generate random characters based on he propmts selected

--- script.js
+++ script.js
@@ -157,7 +157,9 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+    var randomIndex= Math.floor (Math.random () * arr.length)
+    var randomArray= arr[randomIndex]
+  return randomArray
 }
 
 // Function to generate password with user input
